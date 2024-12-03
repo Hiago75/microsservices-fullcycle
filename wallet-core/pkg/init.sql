70845aae3ba060d50333586ad5f4910c7045cb80
Hiago Brenha <hiagobrenha@protonmail.com>
2024-12-03 19:53:20 -0300

    fix: permissions on dbs

--- wallet-core/pkg/init.sql
+++ wallet-core/pkg/init.sql
@@ -1,6 +1,8 @@
+CREATE DATABASE IF NOT EXISTS wallet;
+
 USE wallet;
 
-cCREATE TABLE IF NOT EXISTS clients (
+CREATE TABLE IF NOT EXISTS clients (
     id VARCHAR(255) PRIMARY KEY,
     name VARCHAR(255),
     email VARCHAR(255),
@@ -25,10 +27,18 @@ CREATE TABLE IF NOT EXISTS transactions (
     FOREIGN KEY (account_id_to) REFERENCES accounts(id)
 );
 
-INSERT INTO clients (id, name, email, created_at) VALUES 
-('client1', 'John Doe', 'john@email.com', CURRENT_DATE),
-('client2', 'Jane Doe', 'jane@email.com', CURRENT_DATE);
+INSERT INTO clients (id, name, email, created_at)
+SELECT 'client1', 'John Doe', 'john@email.com', CURRENT_DATE
+WHERE NOT EXISTS (SELECT 1 FROM clients WHERE id = 'client1');
 
-INSERT INTO accounts (id, client_id, balance, created_at) VALUES 
-('account1', 'client1', 1000.00, CURRENT_DATE),
-('account2', 'client2', 500.00, CURRENT_DATE);
+INSERT INTO clients (id, name, email, created_at)
+SELECT 'client2', 'Jane Doe', 'jane@email.com', CURRENT_DATE
+WHERE NOT EXISTS (SELECT 1 FROM clients WHERE id = 'client2');
+
+INSERT INTO accounts (id, client_id, balance, created_at)
+SELECT 'account1', 'client1', 1000.00, CURRENT_DATE
+WHERE NOT EXISTS (SELECT 1 FROM accounts WHERE id = 'account1');
+
+INSERT INTO accounts (id, client_id, balance, created_at)
+SELECT 'account2', 'client2', 500.00, CURRENT_DATE
+WHERE NOT EXISTS (SELECT 1 FROM accounts WHERE id = 'account2');
